2e2107cc22b788c35ecf1e7754e64b3f24a5e2ac
Nathan <dannathan.valeriano@gmail.com>
2025-09-22 12:53:35 +0800

    initial commit

---
+++ MP1.java
@@ -0,0 +1,177 @@
+//authored by Nathan Valeriano
+//A program that creates a health fitness program with tweaks according to specifications.
+import java.util.Scanner;
+
+public class Input_Validation (Scanner scanner, int &x) {
+
+}
+
+public class COR_T1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        //Health Condition Checker
+        System.out.print("Do you have any existing health conditions? (yes/no): ");
+        String healthConditionInput = scanner.nextLine().trim().toLowerCase();
+        if (healthConditionInput.equals("yes")) {
+            System.out.println("\nDue to existing health conditions, it is important to consult a healthcare professional before starting any fitness practices, Safety is a number one priority.");
+            scanner.close();
+            return 0; // Exit
+        }
+        //Inputs from User
+        System.out.println("(Please Provide Necessary Information for accurate results.)")
+        System.out.println("What is your age: ");
+        int age = scanner.nextInt();
+        System.out.println("What is your weight(kg): ");
+        double age = scanner.nextInt();
+        System.out.println("What is your height(cm): ");
+        double age = scanner.nextInt();
+
+        System.out.println("What is your fitness goal? Please indicate your answer by the number beside them. ");
+        System.out.printf("Choices(1-3): %n 1. Lose Weight %n 2. Build %n 3. Maintain %n");
+        int goal = scanner.nextInt();
+        System.out.println("Do you have any Dietary Restrictions? Please indicate your answer by the number beside them.")
+        System.out.printf("Choices(1-6): %n 1. None %n 2. Vegan %n 3. Vegetarian %n 4. Pescatarian %n 5. Gluten-free %n 6. Keto %n");
+        int restriction = scanner.nextInt();
+        System.out.println("How much time are you free for physical activity? (State in hours per week only) ");
+        int exercise_time = scanner.nextInt();
+        System.out.println("Enter your preferred Workout Intensity. ");
+        System.out.printf("Choices(1-3): %n 1. Low %n 2. Moderate %n 3. High %n");
+        int exercise_intensity = scanner.nextInt();
+        System.out.println("Enter your preferred Workout Environment. ");
+        System.out.printf("Choices(1-3): %n 1. Gym %n 2. Home %n 3. Outdoor %n");
+        int exercise_place = scanner.nextInt();
+
+        System.out.println("\nCalculating Results... \n");
+        /* Legend 
+            goal, 1 = Lose, 2 = Build, 3 = Maintain
+            restriction, 1 = None, 2 = Vegan, 3 = Vegetarian, 4 = Pescatarian, 5 = Gluten-free, 6 = Keto
+            time = hrs
+            intensity, 1 = Low, 2 = Moderate, 3 = High
+            place, 1 = gym, 2 = home, 3 = outdoor
+        */
+
+       //BMI
+       double heightM = height/100;
+       String bmi_class = "";
+       double bmi = weight / (heightM*heightM)
+       if (bmi < 18.5) {
+            bmi_class = "Underweight";
+       } else if (bmi < 25) {
+            bmi_class = "Normal Weight";
+       } else if (bmi < 30) {
+            bmi_class = "Overweight";
+       } else {
+            bmi_class = "Obese";
+       }
+
+       //Age-Dependent Caloric Adjustment
+       int calories = 2000;
+       if (age < 25) {
+            calories += 300;
+       } else if (age > 50) {
+            calories -= 300;
+       }
+        //Exercise Recommendation
+        String reco = "";
+        switch (goal) {
+            case 1: //Lose
+                if (bmi =< 25) {
+                    reco = "Intense cardio and strength training is suggested";
+                    calories -= 500;
+                } else {
+                    reco = "Moderate cardio and light weight training";
+                    calories -= 250;
+                } break;
+            case 2: //Build
+                if (time > 7) {
+                    reco = "Heavy weight training with minimal cardio is advised";
+                    calories += 500;
+                } else {
+                    reco = "Moderate weight training with some cardio is recommended";
+                    if (time < 7) {
+                        calories -= 250;
+                    }
+                } break;
+            case 3: //Maintain
+                break;
+            default:
+                System.out.println("If you see this something went wrong");
+                break;
+        }
+        //Environment Preference
+        String reco_loc = "";
+        switch (exercise_place) {
+            case 1: //Gym
+                reco_loc = "Balanced mix of cardio and weight-training regimes";
+                break;
+            case 2: //Home
+                reco_loc = "Body-weight exercises and yoga";
+                break;
+            case 3: //Outdoor
+                reco_loc = ".";
+                break;
+            default:
+                System.out.println("Running and outdoor activities");
+                break;
+        }
+        //Exercise Intensity
+        double water = 2.0; //Daily intake in liters based on Better Health Channel
+        switch (exercise_intensity) {
+            case 1: //Low
+                water += 0.5;
+                if (goal == 2) {
+                    calories -= 100;
+                }
+                break;
+            case 2: //Moderate
+                water += 1;
+                if (goal == 2) {
+                    calories += 100;
+                }
+                break;
+            case 3: //High
+                water += 1.5;
+                if (goal == 2) {
+                    calories += 200;
+                }
+                break;
+            default:
+                System.out.println("Running and outdoor activities");
+                break;
+        }
+        /*Dietary Conditions 
+        restriction,
+        1 = None
+        2 = Vegan
+        3 = Vegetarian
+        4 = Pescatarian
+        5 = Gluten-free
+        6 = Keto*/
+        String reco_dietary = "";
+        switch (restriction) {
+            case 1://None
+
+                break;
+            case 2://Vegan
+
+                break;
+            case 3://Vegetarian
+
+                break;
+            case 4://Pescatarian
+
+                break;
+            case 5://Gluten-free
+
+                break;
+            case 6://Keto
+
+                break;
+            default:
+                System.out.println("Running and outdoor activities");
+                break;
+        }
+
+        scanner.close();
+    }
+}
